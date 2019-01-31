0375c697124299aca141153027dc1e5ea02d715a
R. Kukuh <rkukuh@gmail.com>
2019-01-31 15:38:12 +0700

    Adding multiple screens

---
+++ Multiple Screens/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  Multiple Screens
+//
+//  Created by R. Kukuh on 31/01/19.
+//  Copyright © 2019 R. Kukuh. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
